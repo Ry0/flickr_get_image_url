dfde23d02065c83cbdae69ffea64bb46effe1005
Ry0 <k104037r@mail.kyutech.jp>
2015-10-07 21:09:24 +0900

    希望するサイズがなかったときにそれよりも小さいサイズで存在するかどうか確認するようにした

--- flickr_get_image_url.py
+++ flickr_get_image_url.py
@@ -79,18 +79,38 @@ class Flickr(object):
                 pass
         return result
 
+    def check_exist_image(self, photo_id, hope_image_size_index):
+        flag = False
+        for i in reversed(range(0, hope_image_size_index)):
+            image_url = self.get_url_from_photo_id(photo_id, self.image_size[i])
+            if (image_url != None):
+                print self.image_size[hope_image_size_index] + "のサイズは存在しません．" + self.image_size[i] + "サイズを取得します．"
+                self.snippet = self.snippet.replace(self.image_snippet[hope_image_size_index], image_url)
+                flag = True
+                break
+
+        if (flag == False):
+            print "指定したIDの画像は存在しません"
+            exit()
+
     # 指定したスニペットから指定した画像サイズのURLに置換
     def convert_snippet(self, photo_id):
-        for (num_image_size, num_image_snippet) in zip(self.image_size, self.image_snippet):
-
-            if self.snippet.find(num_image_snippet)!=-1:
-                image_url = f.get_url_from_photo_id(photo_id, num_image_size)
-                if(image_url == None):
-                    print "指定したIDの画像は存在しません"
-                    exit()
+        # for (num_image_size, num_image_snippet) in zip(self.image_size, self.image_snippet):
+        for i in range(0, 11):
+            if (self.snippet.find(self.image_snippet[i])!=-1):
+                image_url = self.get_url_from_photo_id(photo_id, self.image_size[i])
+                if (image_url == None):
+                    # image_url = self.get_url_from_photo_id(photo_id, 'Original')
+                    # if(image_url == None):
+                    #   print "指定したIDの画像は存在しません"
+                    #   exit()
+                    # else :
+                    #   print str(num_image_size) + "のサイズは存在しません．オリジナルサイズを取得します"
+                    #   self.snippet = self.snippet.replace(num_image_snippet, image_url)
+                    self.check_exist_image(photo_id, i)
                 else:
-                    print str(num_image_size) + "の画像を取得中..."
-                    self.snippet = self.snippet.replace(num_image_snippet, image_url)
+                    print self.image_size[i] + "の画像を取得中..."
+                    self.snippet = self.snippet.replace(self.image_snippet[i], image_url)
         return self.snippet
 
 
